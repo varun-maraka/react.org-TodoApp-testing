2cc02496808126f51fc28e09598748aea753460a
varun-maraka <varun.maraka@gmail.com>
2020-12-24 14:37:30 -0600

    Initial

--- src/App.js
+++ src/App.js
@@ -1,9 +1,73 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import ReactDOM from "react-dom";
+class TodoApp extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = { items: [], text: '' };
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
+  
+    render() {
+      return (
+        <div>
+          <h3>TODO</h3>
+          <TodoList items={this.state.items} />
+          <form onSubmit={this.handleSubmit}>
+            <label htmlFor="new-todo">
+              What needs to be done?
+            </label>
+            <input
+              id="new-todo"
+              onChange={this.handleChange}
+              value={this.state.text}
+            />
+            <button>
+              Add #{this.state.items.length + 1}
+            </button>
+          </form>
+        </div>
+      );
+    }
+  
+    handleChange(e) {
+      this.setState({ text: e.target.value });
+    }
+  
+    handleSubmit(e) {
+      e.preventDefault();
+      if (this.state.text.length === 0) {
+        return;
+      }
+      const newItem = {
+        text: this.state.text,
+        id: Date.now()
+      };
+      this.setState(state => ({
+        items: state.items.concat(newItem),
+        text: ''
+      }));
+    }
+  }
+  
+  class TodoList extends React.Component {
+    render() {
+      return (
+        <ul>
+          {this.props.items.map(item => (
+            <li key={item.id}>{item.text}</li>
+          ))}
+        </ul>
+      );
+    }
+  }
 
 function App() {
   return (
     <div className="App">
+      <TodoApp />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -21,5 +85,9 @@ function App() {
     </div>
   );
 }
+
+
+
+
 
 export default App;
