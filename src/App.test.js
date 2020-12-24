c4c486efd8b3a8431e547dd58ecf479388e7e2d6
varun-maraka <varun.maraka@gmail.com>
2020-12-24 15:46:15 -0600

    Added first react test case

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
-
+import ReactDOM from "react-dom"; 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test("renders the correct content",()=>{
+  const root = document.createElement("div");
+  ReactDOM.render(<App/>,root);
+  expect(root.querySelector("h3").textContent).toBe("TODO");
+})
